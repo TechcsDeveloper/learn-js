b8641daa0439e0c3be3fd9c7515fd0c61eb99a66
BABULAL MANDAL <babulalmandal678@gmail.com>
2024-08-25 18:56:33 +0530

    Variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+var firstName = "Babulal";
+let lastName = "Mandal";
+const rollNumber = 1548;
+let accountState;
+
+console.log(firstName, lastName, rollNumber);
+console.table([firstName, lastName, rollNumber, accountState]);
+
+/* prefer not to use var because of issue in block scope and functional scope */
