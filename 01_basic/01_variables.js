7626bed015814fd2370ee118b399c95874ea317c
BABULAL MANDAL <babulalmandal678@gmail.com>
2024-08-25 12:33:50 +0530

    Variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "babulal@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 not allowed
+
+accountEmail = "babulal@gmail.com"
+accountPassword = "21586494"
+accountCity = "Bengaluru"
+
+console.log(accountEmail);
+console.table(accountId, accountEmail, accountPassword, accountCity);
--- 01_var.js
+++
@@ -1,2 +0,0 @@
-let name = "Babulal";
-console.log(name);
