3ecf7ee145a0363dc6aa0540ce0844dd69fe86a8
BABULAL MANDAL <babulalmandal678@gmail.com>
2024-08-25 19:12:22 +0530

    Data Types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3) // we are using nodejs, not browser
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not defined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); //output would be undefined
+console.log(typeof null); //output would be object
